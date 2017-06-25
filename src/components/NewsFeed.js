4fa850c5b5d2cf4b2641a43bfc55b3b5f795f8bc
Tiffany Tse <ttse93@gmail.com>
2017-06-24 19:56:27 -0700

    Started on NewsFeed Component, there is some functionality to rener

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,12 +1,37 @@
 /** 
 *  NewsFeed.js
-*  Description: shows list view
+*  Description: shows list view only works for ios
 *  Modules: React and React Native Modules
 *  Dependencies: styles.js Title
 *  Author: Tiffany Tse
 */
 //import modules
 import React, { PropTypes } from 'react';
-import { StyleSheet, Image, View } from 'react-native';
+import { ListView, StyleSheet, View } from 'react-native';
 import Title from './Title.js';
 import * as globalStyles from '../styles/global.js';
+export default class NewsFeed extends Component {
+    constructor (props) {
+        super(props);
+        this.dataSource = new ListView DataStore({
+            //compare two rows to tell if they have changed by comparing the title
+            rowHasChanged: (row1, row2) => row1.title !== row2.title
+        });
+        //call state
+        this.state = {
+            dataSource: this.dataSource.cloneWithRows(props.news)
+        };
+    }
+    render () {
+        return (
+            <View div={globalStyles.COMMON_STYLES.pageContainer> 
+                <ListView />
+            </View>
+        );
+    }
+};
+//define propTypes
+NewsFeed.propTypes = {
+    news: PropTypes.arrayOf(PropTypes.object),
+    listStyles: View.propTypes.style
+};
