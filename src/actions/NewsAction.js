faca81304f4e3ef6934290e736690a074741cd32
Tiffany Tse <ttse@stainlesscode.com>
2017-07-29 14:34:51 -0700

    Add exported const loadNews and searchNews in NewsAction.js

--- src/actions/NewsAction.js
+++ src/actions/NewsAction.js
@@ -4,3 +4,17 @@
  * Author: Tiffany Tse
  * Created: July 29, 2017
  */
+
+//import modules
+import { LOAD_NEWS, SEARCH_NEWS } from './ActionTypes';
+import mockData from '../TestData.json';
+
+export const loadNews = () => ({
+    type: LOAD_NEWS,
+    payload: mockData
+});
+
+export const searchNews = searchTerm => ({
+    type: SEARCH_NEWS,
+    payload: searchTerm
+});
