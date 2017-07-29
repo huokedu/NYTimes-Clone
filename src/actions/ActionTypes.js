fd87fc6247591f42149b9d7c93586a29ee3550ac
Tiffany Tse <ttse@stainlesscode.com>
2017-07-29 12:53:25 -0700

    Create Actions/ along with NewsActions.js and ActionTypes.js

---
+++ src/actions/ActionTypes.js
@@ -0,0 +1,8 @@
+/* ActionTyps.js
+ * create actions
+ * Dependencies: Redux, redux-logger, newsFeedReducer, searchTermReducer
+ * Author: Tiffany Tse
+ * Created: July 16, 2017
+ */
+export const LOAD_NEWS = 'LOAD_NEWS';
+export const SEARCH_NEWS = 'SEARCH_NEWS';
---
+++ src/actions/NewsAction.js
@@ -0,0 +1,6 @@
+/* ActionTyps.js
+ * create actions
+ * Dependencies: Redux, redux-logger, newsFeedReducer, searchTermReducer
+ * Author: Tiffany Tse
+ * Created: July 16, 2017
+ */
