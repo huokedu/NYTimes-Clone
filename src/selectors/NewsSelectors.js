ef62f54c89a9ab22e1d4557078ba9643b8a1a661
Tiffany Tse <ttse@stainlesscode.com>
2017-08-12 16:22:53 -0700

    init selectors/NewsSelectors.js

---
+++ src/selectors/NewsSelectors.js
@@ -0,0 +1,29 @@
+/* NewsSelector.js
+ * Use reselect.js library for our seearch functionlity which has two types of selectors: input and memoized
+ * Dependencies: ActionTypes
+ * Modules: NewsActions, and NewsFeed
+ * Author: Tiffany Tse
+ * Created: July 29, 2017
+ */
+//import dependencies
+import {createSelector } from 'reselect';
+//import modules
+import { reshapeNewsData } from '../util/DataTransformations.js';
+
+
+//pass state  to newsSelector where wstate.news is passed to reshapeNewsData as input
+const newsSelector = state => state.news;
+
+/*Return the news portion of our Redux state tree. From there, we create two memoized
+selectors–reshapeNewsSelector and allNewsSelector. Memoized selectors take two
+arguments. The first is an array of inputs or memoized selectors.*/
+const reshapeNewsSelector = createSelector(
+    [newsSelector],
+    reshapeNewsData
+);
+
+//allNewsSelector simply returns the entire transformed list without modification (newsItems => newsItems).
+export const allNewsSelector = createSelector(
+    [reshapeNewsSelector],
+    newsItems => newsItems
+);
