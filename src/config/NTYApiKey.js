2adc9a08e4aa3d2267fe0e9040d126a8482d1bcd
Tiffany Tse <ttse@stainlesscode.com>
2017-08-12 16:29:34 -0700

    init NTYApiKey.js

---
+++ src/config/NTYApiKey.js
@@ -0,0 +1,10 @@
+/* NYTApiKey.js
+ * file that contains my developer api key
+ * Dependencies: none
+ * Modules: none
+ * Author: Tiffany Tse
+ * Created: August 12, 2017
+ */
+
+const NYT_API_KEY = '49d9eabb1a8f4053a733f43d3c3084b9';
+export default NYT_API_KEY;
